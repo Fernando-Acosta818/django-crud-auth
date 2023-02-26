961c3e8ab81d9e7620e14bc146558c71f21163a6
Fernando Acosta <fer.acsvi@gmail.com>
2023-02-25 19:08:41 -0600

    Validacion y correcciones

--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.contrib.auth import login
+from django.db import IntegrityError
 
 def home(request):
     return render(request, 'home.html')
 
 def signup(request):
-
     if request.method == 'GET':
         return render(request, 'signup.html', {
             'form' : UserCreationForm
@@ -17,8 +17,9 @@ def signup(request):
             try:
                 user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
                 user.save()
-                return HttpResponse('User created succesfully')
-            except:
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
                 return render(request, 'signup.html', {
                     'form' : UserCreationForm,
                     'error' : 'Username already exists'
@@ -28,5 +29,5 @@ def signup(request):
             'error' : 'passwords do not match'
         })
 
-    
-
+def tasks(request):
+    return render(request, 'tasks.html')
